0503a61ee2c32a2240f3521c2fb3fe06f0225a12
Blake Stephens <blake.stephens@lge.com>
2019-07-02 16:35:40 -0700

    Converted Icon to material icons
    Enact-DCO-1.0-Signed-off-by: Blake Stephens <blake.stephens@lge.com>

--- Icon/Icon.js
+++ Icon/Icon.js
@@ -18,12 +18,13 @@ import PropTypes from 'prop-types';
 
 import Skinnable from '../Skinnable';
 
-import iconList from './IconList.js';
-
 import componentCss from './Icon.module.less';
 
 /**
  * Renders a MyTheme-styled icon without any behavior.
+ *
+ * my-theme uses Material icons out-of-the-box. For a complete list of supported icons, visit the
+ * material icons website: https://material.io/tools/icons/
  *
  * @class IconBase
  * @memberof my-theme/Icon
@@ -51,103 +52,16 @@ const IconBase = kind({
 		children: PropTypes.oneOfType([PropTypes.string, PropTypes.object])
 	},
 
+	styles: {
+		css: componentCss,
+		className: 'material-icons'
+	},
+
 	render: (props) => UiIcon.inline({
 		...props,
-		css: componentCss,
-		iconList
+		css: props.css
 	})
 });
-
-// Let's find a way to import this list directly, and bonus feature, render our icons in the docs
-// next to their names.
-/**
- * An object whose keys can be used as the child of an [Icon]{@link my-theme/Icon.Icon} component.
- *
- * List of Icons:
- * ```
- * plus
- * minus
- * arrowhookleft
- * arrowhookright
- * ellipsis
- * check
- * circle
- * stop
- * play
- * pause
- * forward
- * backward
- * skipforward
- * skipbackward
- * pauseforward
- * pausebackward
- * pausejumpforward
- * pausejumpbackward
- * jumpforward
- * jumpbackward
- * denselist
- * bulletlist
- * list
- * drawer
- * arrowlargedown
- * arrowlargeup
- * arrowlargeleft
- * arrowlargeright
- * arrowsmallup
- * arrowsmalldown
- * arrowsmallleft
- * arrowsmallright
- * closex
- * search
- * rollforward
- * rollbackward
- * exitfullscreen
- * fullscreen
- * arrowshrinkleft
- * arrowshrinkright
- * arrowextend
- * arrowshrink
- * flag
- * funnel
- * trash
- * star
- * hollowstar
- * halfstar
- * gear
- * plug
- * lock
- * forward15
- * back15
- * continousplay
- * playlist
- * resumeplay
- * image
- * audio
- * music
- * languages
- * cc
- * ccon
- * ccoff
- * sub
- * recordings
- * livezoom
- * liveplayback
- * liveplaybackoff
- * repeat
- * repeatoff
- * series
- * repeatdownload
- * view360
- * view360off
- * info
- * ```
- *
- * @name iconList
- * @memberof my-theme/Icon
- * @constant
- * @type {Object}
- * @public
- */
 
 /**
  * MyTheme-specific behaviors to apply to [IconBase]{@link my-theme/Icon.IconBase}.
@@ -177,6 +91,5 @@ export default Icon;
 export {
 	Icon,
 	IconBase,
-	IconDecorator,
-	iconList as icons
+	IconDecorator
 };
