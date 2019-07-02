6d654612a4955882b5e2428c0d6b3419d0e71cf8
Dave Freeman <dave.freeman@lge.com>
2019-07-02 17:50:57 -0500

    remove extraneous props from Panels and Panel

--- Panels/Panel.js
+++ Panels/Panel.js
@@ -4,14 +4,11 @@ import kind from '@enact/core/kind';
 import React from 'react';
 import PropTypes from 'prop-types';
 import Slottable from '@enact/ui/Slottable';
-import Spotlight from '@enact/spotlight';
 import SpotlightContainerDecorator, {spotlightDefaultClass} from '@enact/spotlight/SpotlightContainerDecorator';
 
 import Skinnable from '../Skinnable';
 
 import componentCss from './Panel.module.less';
-
-let panelId = 0;
 
 /**
  * A Panel is the standard view container used inside a [Panels]{@link my-theme/Panels.Panels} view
@@ -29,32 +26,6 @@ const PanelBase = kind({
 
 	propTypes: /** @lends my-theme/Panels.Panel.prototype */ {
 		/**
-		 * By default, the panel will be labeled by its [Header]{@link my-theme/Panels.Header}.
-		 * When `aria-label` is set, it will be used instead to provide an accessibility label for
-		 * the panel.
-		 *
-		 * @memberof my-theme/Panels.Panel.prototype
-		 * @type {String}
-		 * @public
-		 */
-		'aria-label': PropTypes.string,
-
-		/**
-		 * Sets the strategy used to automatically focus an element within the panel upon render.
-		 *
-		 * * "none" - Automatic focus is disabled
-		 * * "last-focused" - The element last focused in the panel with be restored
-		 * * "default-element" - The first spottable component within the body will be focused
-		 * * Custom Selector - A custom CSS selector may also be provided which will be used to find
-		 *   the target within the Panel
-		 *
-		 * @type {String}
-		 * @default 'last-focused'
-		 * @public
-		 */
-		autoFocus: PropTypes.string,
-
-		/**
 		 * Header for the panel.
 		 *
 		 * This can also be passed by the [Slottable]{@link ui/Slottable.Slottable} API by using a
@@ -63,27 +34,7 @@ const PanelBase = kind({
 		 * @type {Node}
 		 * @public
 		 */
-		header: PropTypes.node,
-
-		/**
-		 * Hides the body components.
-		 *
-		 * When a Panel is used within [`Panels`]{@link my-theme/Panels.Panels},
-		 * [`ActivityPanels`]{@link my-theme/Panels.ActivityPanels}, or
-		 * [`AlwaysViewingPanels`]{@link my-theme/Panels.AlwaysViewingPanels},
-		 * this property will be set automatically to `true` on render and `false` after animating
-		 * into view.
-		 *
-		 * @type {Boolean}
-		 * @default false
-		 * @public
-		 */
-		hideChildren: PropTypes.bool
-	},
-
-	defaultProps: {
-		autoFocus: 'last-focused',
-		hideChildren: false
+		header: PropTypes.node
 	},
 
 	styles: {
@@ -99,66 +50,26 @@ const PanelBase = kind({
 				currentTarget.scrollTop = 0;
 				currentTarget.scrollLeft = 0;
 			}
-		),
-		spotOnRender: (node, {autoFocus}) => {
-			if (node && !Spotlight.getCurrent()) {
-				const {spotlightId} = node.dataset;
-				const config = {
-					enterTo: 'last-focused'
-				};
-
-				if (autoFocus !== 'last-focused') {
-					config.enterTo = 'default-element';
-
-					if (autoFocus !== 'default-element') {
-						config.defaultElement = autoFocus;
-					}
-				}
-
-				Spotlight.set(spotlightId, config);
-				Spotlight.focus(spotlightId);
-			}
-		}
+		)
 	},
 
 	computed: {
-		spotOnRender: ({autoFocus, hideChildren, spotOnRender}) => {
-			// In order to spot the body components, we defer spotting until !hideChildren. If the
-			// Panel opts out of hideChildren support by explicitly setting it to false, it'll spot
-			// on first render.
-			if (hideChildren || autoFocus === 'none') {
-				return null;
-			}
-
-			return spotOnRender;
-		},
-		children: ({children, hideChildren}) => hideChildren ? null : children,
-		bodyClassName: ({header, hideChildren, styler}) => styler.join({
+		bodyClassName: ({header, styler}) => styler.join({
 			body: true,
-			noHeader: !header,
-			visible: !hideChildren
-		}),
-		// nulling headerId prevents the aria-labelledby relationship which is necessary to allow
-		// aria-label to take precedence
-		// (see https://www.w3.org/TR/wai-aria/states_and_properties#aria-labelledby)
-		headerId: ({'aria-label': label}) => label ? null : `panel_${++panelId}_header`
+			noHeader: !header
+		})
 	},
 
-	render: ({bodyClassName, css, children, header, headerId, spotOnRender, ...rest}) => {
-		delete rest.autoFocus;
-		delete rest.hideChildren;
+	render: ({bodyClassName, css, children, header, ...rest}) => {
 
 		return (
 			<Column
 				component="article"
 				role="region"
 				{...rest}
-				aria-labelledby={headerId}
-				ref={spotOnRender}
 			>
 				<Cell
 					className={css.header}
-					id={headerId}
 					shrink
 				>
 					{header}
@@ -176,10 +87,8 @@ const PanelBase = kind({
 
 const Panel = SpotlightContainerDecorator(
 	{
-		// prefer any spottable within the panel body for first render
-		continue5WayHold: true,
+		// this `defaultElement` selector will prefer any spottable within the panel body for first render
 		defaultElement: [`.${spotlightDefaultClass}`, `.${componentCss.body} *`],
-		enterTo: 'last-focused',
 		preserveId: true
 	},
 	Slottable(
